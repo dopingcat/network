bfdb12652b8b480632644c289f0d3e6421d8754e
vwan <dopingcat@nate.com>
2015-11-30 13:03:32 +0900

    add InetAddressExample01

---
+++ src/com/hanains/network/test/ex/InetAddressExample01.java
@@ -0,0 +1,27 @@
+package com.hanains.network.test.ex;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class InetAddressExample01 {
+	public static void main(String[] args) {
+		try {
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			System.out.println(inetAddress);
+
+			System.out.println(inetAddress.getHostName());
+			
+			System.out.println(inetAddress.getHostAddress());
+			
+			for(int b : inetAddress.getAddress()) {
+				System.out.print(b & 0xff);
+				System.out.print(".");
+			}
+			System.out.println();
+			
+			System.out.println(Integer.toHexString(inetAddress.hashCode()));
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+}
