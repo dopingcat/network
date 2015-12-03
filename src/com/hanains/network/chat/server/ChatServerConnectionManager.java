c83e2989c498226e2a1fbbd617840c96bc7a7ca6
vwan <dopingcat@nate.com>
2015-12-03 16:55:24 +0900

    impl ChatUtil

--- src/com/hanains/network/chat/server/ChatServerConnectionManager.java
+++ src/com/hanains/network/chat/server/ChatServerConnectionManager.java
@@ -38,7 +38,7 @@ public class ChatServerConnectionManager {
 		System.out.println("===============================================================================");
 	}
 	
-	public void addConnection(ChatConnection chatConnection) throws IOException {
+	public void addConnection(ChatConnection chatConnection) {
 		connectionPool.put(chatConnection.getName(), chatConnection);
 		joinRoom("lobby", chatConnection.getName());
 	}
@@ -77,13 +77,10 @@ public class ChatServerConnectionManager {
 		roomInfo.put(roomName, new LinkedList<String>());
 	}
 	
-	public void joinRoom(String roomName, String userName) throws IOException {
+	public void joinRoom(String roomName, String userName) {
 		if(!roomInfo.containsKey(roomName)) {
 			createRoom(roomName);
 		}
-		
-		ChatUtil.getChatUtil().roomcast(roomName, ("======================= '" + userName + "'님이 입장하셨습니다. ======================="));
-		
 		roomInfo.get(connectionPool.get(userName).getLocale()).remove(userName); // 로비에서 삭제
 		roomInfo.get(roomName).add(userName);	// 참여방에 추가
 		connectionPool.get(userName).setLocale(roomName);	// 장소 변경
